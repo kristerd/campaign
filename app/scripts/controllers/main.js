659741d35577539bde4a4a56fad360fe381628cb
drikrimt <kd@mobiletech.no>
2013-10-04 12:31:47 +0200

    Added angular-fire to bower.json

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,10 +3,16 @@
 angular.module('campaignApp')
   .controller('MainCtrl', function ($scope) {
    
-  	var chatRef = new Firebase('https://campaign.firebaseio.com/');
+  	var chatRef = new Firebase('https://campaign.firebaseio.com');
+
 	var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
+
 		console.log(error);
 		console.log(user);
+	});
+
+	auth.login('facebook', {
+	  	rememberMe: true
 	});
 
   });
