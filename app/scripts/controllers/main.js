674a2af009d073979fb3f6ff61631873ad39fb23
drikrimt <kd@mobiletech.no>
2013-10-04 13:28:07 +0200

    Login with e-mail

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,17 +2,29 @@
 
 angular.module('campaignApp')
   .controller('MainCtrl', function ($scope) {
+
+  	$scope.user;
    
   	var chatRef = new Firebase('https://campaign.firebaseio.com');
 
 	var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
-
-		console.log(error);
-		console.log(user);
+		if(user) {
+			console.log(user);
+			console.log("Logged In");
+		}
+		else {
+			console.log("Not Logged In");
+		}
 	});
 
-	auth.login('facebook', {
-	  	rememberMe: true
-	});
+	$scope.logIn = function() {
+		auth.login('password', {
+		  email: $scope.user.email,
+		  password: $scope.user.password
+		});
+	}
 
+	$scope.logOut = function() {
+		auth.logout();
+	}
   });
