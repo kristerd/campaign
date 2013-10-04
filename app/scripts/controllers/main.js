64557db20f5831fc8a6d0e00e3fd10a8124b8902
drikrimt <kd@mobiletech.no>
2013-10-04 10:41:31 +0200

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('campaignApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
